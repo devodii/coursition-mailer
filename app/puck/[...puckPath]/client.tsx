5979fab2140f5b0c4682b02eee0ba10c98cc5921
Emmanuel odii <emmanuelodii80@gmail.com>
2024-01-07 14:28:03 +0100

    perf: update editor's header

--- app/puck/[...puckPath]/client.tsx
+++ app/puck/[...puckPath]/client.tsx
@@ -3,9 +3,27 @@
 import type { Data } from "@measured/puck";
 import { Puck } from "@measured/puck";
 import config from "../../../puck.config";
-import { Schedule } from "@components/schedule-button";
+import { __isBrowser__ } from "lib/constant";
 
-const isBrowser = typeof window !== "undefined";
+function handleShedule(data: any) {
+  console.log({ data });
+}
+
+function Schedule() {
+  const content = __isBrowser__
+    ? JSON.parse(localStorage.getItem("data")!)
+    : {};
+  return (
+    <div className="w-screen flex items-end justify-end py-5">
+      <button
+        onClick={() => handleShedule(content)}
+        className="bg-blue-500 w-full max-w-[200px] px-3 py-1.5 mr-5"
+      >
+        Schedule
+      </button>
+    </div>
+  );
+}
 
 export function Client({ data }: { data: Data }) {
   return (
---
+++ lib/constant.ts
@@ -0,0 +1,1 @@
+export const __isBrowser__ = typeof window !== "undefined";
